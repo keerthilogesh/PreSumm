770aa93d222537852eb0567075cd85980d2c0c0a
Keerthana Murugaraj keerthana.murugaraj.001@student.uni.lu <kmurugaraj@access2.iris-cluster.uni.lux>
2022-11-02 20:17:55 +0100

    retrain model three configs

--- retrain_model.sh
+++ retrain_model.sh
@@ -4,22 +4,31 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH -c 10
 #SBATCH -G 4
-#SBATCH --time=05:00:00
+#SBATCH --time=10:00:00
 #SBATCH -p gpu
 #SBATCH -o gpu_hist_bert.out
 
 # retrain from historical data (histbert)
-bert_data_path = ../data/historical_samples/bert_data/bert.pt_data/
-model_data_path = ../data/historical_samples/model_path/
-log_file_path = ../logs/train_historical_data_from_pretrained.log
-train_from_path = ../data/baseline_model/model_path/bertext_cnndm_transformer.pt
+#bert_data_path='../data/historical_samples/bert_data/bert.pt_data/'
+#model_data_path='../data/historical_samples/basic_model_path/'
+#log_file_path='../logs/train_historical_data_from_pretrained.log'
+#train_from_path='../data/baseline_model/bertext_cnndm_transformer.pt'
+#train_steps=50000
 
-# retrain from cnn data (histbert)
-bert_data_path = ../data/cnn/bert_data/bert.pt_data/cnndm/
-model_data_path = ../data/cnn/model_path/
-log_file_path = ../logs/train_cnn_data_from_pretrained.log
-train_from_path = ../data/baseline_model/model_path/bertext_cnndm_transformer.pt
+# 2: retrain from cnn data (histbert)
+#bert_data_path='../data/cnn/bert_data/cnndm/cnndm.'
+#model_data_path='../data/cnn/model_path/'
+#log_file_path='../logs/train_cnn_data_from_pretrained.log'
+#train_from_path='../data/baseline_model/bertext_cnndm_transformer.pt'
+#train_steps=50000
+
+# 3: retrain from historical data - initialize with 2
+bert_data_path='../data/historical_samples/bert_data/bert.pt_data/'
+model_data_path='../data/historical_samples/pre_model_path/'
+log_file_path='../logs/train_historical_data_from_pretrained_hist.log'
+train_from_path='../data/cnn/model_path/model_step_50000.pt'
+train_steps=70000
 
 cd /home/users/kmurugaraj/masterthesis/PreSumm/src
 conda activate histsumm_gpu
-python train.py -task ext -mode train -bert_data_path $bert_data_path -ext_dropout 0.1 -model_path $model_data_path -lr 2e-3 -visible_gpus 0,1,2,3 -report_every 50 -save_checkpoint_steps 1000 -batch_size 300 -train_steps 50000 -accum_count 2 -log_file $log_file_path -use_interval true -warmup_steps 10000 -max_pos 512 -model histbert -train_from $train_from_path
+python train.py -task ext -mode train -bert_data_path $bert_data_path -ext_dropout 0.1 -model_path $model_data_path -lr 2e-3 -visible_gpus 0,1,2,3 -report_every 50 -save_checkpoint_steps 1000 -batch_size 300 -train_steps $train_steps -accum_count 2 -log_file $log_file_path -use_interval true -warmup_steps 10000 -max_pos 512 -model histbert -train_from $train_from_path
