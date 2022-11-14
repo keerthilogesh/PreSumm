98f6cbf8f1b0594b9901e0f6ff10431ed4d97638
Keerthana Murugaraj keerthana.murugaraj.001@student.uni.lu <kmurugaraj@access2.iris-cluster.uni.lux>
2022-11-14 20:20:43 +0100

    abs slurm script

--- slurm_scripts/bertabs_cnndm_transformer_retrain_model.sh
+++ slurm_scripts/bertabs_cnndm_transformer_retrain_model.sh
@@ -4,29 +4,29 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH -c 10
 #SBATCH -G 4
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH -p gpu
-#SBATCH -o gpu_hist_bert.out
+#SBATCH -o gpu_hist_bert_abs3.out
 
 # retrain from historical data (histbert)
-bert_data_path='../data/historical_samples/bert_data/bert.pt_data/'
-model_data_path='../data/historical_samples/abstractive/basic_model_path/'
-log_file_path='../logs/bertabs_train_historical_data_from_pretrained.log'
-train_from_path='../data/baseline_model/bertextabs_cnndm_final_148000.pt'
-train_steps=200000
+# bert_data_path='../data/historical_samples/bert_data/bert.pt_data/'
+# model_data_path='../data/historical_samples/abstractive/basic_model_path/'
+# log_file_path='../logs/bertabs_train_historical_data_from_pretrained.log'
+# train_from_path='../data/baseline_model/bertextabs_cnndm_final_148000.pt'
+# train_steps=200000
 
 # 2: retrain from cnn data (histbert)
-bert_data_path='../data/cnn/bert_data/cnndm/cnndm.'
-model_data_path='../data/cnn/model_path/abstractive/'
-log_file_path='../logs/bertabs_train_cnn_data_from_pretrained.log'
-train_from_path='../data/baseline_model/bertextabs_cnndm_final_148000.pt'
-train_steps=200000
+# bert_data_path='../data/cnn/bert_data/cnndm/cnndm.'
+# model_data_path='../data/cnn/model_path/abstractive/'
+# log_file_path='../logs/bertabs_train_cnn_data_from_pretrained.log'
+# train_from_path='../data/baseline_model/bertextabs_cnndm_final_148000.pt'
+# train_steps=200000
 
 # 3: retrain from historical data - initialize with 2
 bert_data_path='../data/historical_samples/bert_data/bert.pt_data/'
 model_data_path='../data/historical_samples/abstractive/pre_model_path/'
 log_file_path='../logs/bertabs_train_historical_data_from_pretrained_hist.log'
-train_from_path='../data/cnn/model_path/abstractive/model_step_50000.pt'
+train_from_path='../data/cnn/model_path/abstractive/model_step_200000.pt'
 train_steps=250000
 
 cd /home/users/kmurugaraj/masterthesis/PreSumm/src
